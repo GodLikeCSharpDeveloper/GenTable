e8db7d9025887661836d564a5562ca0ca9c27d3c
GodLikeCSharpDeveloper <baf241@gmail.com>
2023-03-02 23:36:42 +0200

    mnhjmb

---
+++ BlazorStoreServAppV5/Repository/StoreLogic/OrderRepository/GenLinqExt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Linq.Expressions;
+
+namespace BlazorStoreServAppV5.Repository.StoreLogic.OrderRepository
+{
+    public static class GenLinqExt
+    {
+        public static IEnumerable OrderBy<TEntity>(this IQueryable<TEntity> source, string orderByProperty, bool desc)
+        {
+            string command = desc ? "OrderByDescending" : "OrderBy";
+            var type = typeof(TEntity);
+            var property = type.GetProperty(orderByProperty);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+            var resultExpression = Expression.Call(typeof(Queryable), command, new Type[] { type, property.PropertyType },
+                source.Expression, Expression.Quote(orderByExpression));
+            return (IOrderedQueryable<TEntity>)source.Provider.CreateQuery<TEntity>(resultExpression);
+        }
+    }
+}
