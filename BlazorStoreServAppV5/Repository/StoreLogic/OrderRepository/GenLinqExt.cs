5b0b7964364ebe4b18cecc8c541f54f0768f38b1
GodLikeCSharpDeveloper <125124478+GodLikeCSharpDeveloper@users.noreply.github.com>
2023-03-03 16:35:15 +0200

    efefef

--- BlazorStoreServAppV5/Repository/StoreLogic/OrderRepository/GenLinqExt.cs
+++ BlazorStoreServAppV5/Repository/StoreLogic/OrderRepository/GenLinqExt.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace BlazorStoreServAppV5.Repository.StoreLogic.OrderRepository
 {
     public static class GenLinqExt
     {
-        public static IEnumerable OrderBy<TEntity>(this IQueryable<TEntity> source, string orderByProperty, bool desc)
+        public static IQueryable<TEntity> OrderBy<TEntity>(this IQueryable<TEntity> source, string orderByProperty, bool desc)
         {
             string command = desc ? "OrderByDescending" : "OrderBy";
             var type = typeof(TEntity);
-            var property = type.GetProperty(orderByProperty);
             var parameter = Expression.Parameter(type, "p");
-            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
-            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
-            var resultExpression = Expression.Call(typeof(Queryable), command, new Type[] { type, property.PropertyType },
-                source.Expression, Expression.Quote(orderByExpression));
+            Expression body = parameter;
+            foreach (var member in orderByProperty.Split('.'))
+            {
+                body = Expression.PropertyOrField(body, member);
+            }
+            var orderByExpression = Expression.Lambda(body, parameter);
+            var resultExpression = Expression.Call(typeof(Queryable), command, new Type[] { type, body.Type },
+                  source.Expression, Expression.Quote(orderByExpression));
             return (IOrderedQueryable<TEntity>)source.Provider.CreateQuery<TEntity>(resultExpression);
+            
         }
     }
 }
