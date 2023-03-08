632afd9f9b462e3713353534c9950f06b3be8ac7
GodLikeCSharpDeveloper <125124478+GodLikeCSharpDeveloper@users.noreply.github.com>
2023-03-08 16:46:44 +0200

    fff

---
+++ BlazorStoreServAppV5/Models/BLogicModel/WrapperForOrders.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorStoreServAppV5.Models.BLogicModel
+{
+    public class WrapperForOrders
+    {
+        public OrderModel orders;
+        public bool OrderBool { get; set; }
+        public WrapperForOrders(OrderModel orders)
+        {
+            this.orders = orders;
+        }
+    }
+}
