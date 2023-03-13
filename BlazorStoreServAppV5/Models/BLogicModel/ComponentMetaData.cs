a871adf5ad1c55d83240b1380091d5093697fc3c
GodLikeCSharpDeveloper <baf241@gmail.com>
2023-03-13 22:20:11 +0200

    куп

---
+++ BlazorStoreServAppV5/Models/BLogicModel/ComponentMetaData.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorStoreServAppV5.Models.BLogicModel;
+
+public class ComponentMetaData
+{
+    public string? Name { get; set; }
+    public Dictionary<string, object> Parameters { get; set; } = new();
+}
