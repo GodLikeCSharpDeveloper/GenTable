6b4bb6dd315faea160747409933ead93f5183437
GodLikeCSharpDeveloper <125124478+GodLikeCSharpDeveloper@users.noreply.github.com>
2023-03-13 16:53:26 +0200

    7

--- BlazorStoreServAppV5/Repository/StoreLogic/SearchRepository/SearchLucene.cs
+++ BlazorStoreServAppV5/Repository/StoreLogic/SearchRepository/SearchLucene.cs
@@ -1,16 +1,9 @@
-﻿using System.Security.Policy;
-using Lucene.Net.Analysis.Standard;
-using Lucene.Net.Analysis;
-using Lucene.Net.Analysis.Core;
+﻿using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
-using Lucene.Net.QueryParsers;
 using Lucene.Net.QueryParsers.Classic;
 using Lucene.Net.Search;
 using Lucene.Net.Store;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Directory = Lucene.Net.Store.Directory;
-using Lucene.Net.Index.Extensions;
 using Lucene.Net.Search.Spell;
 using Lucene.Net.Util;
 
@@ -28,7 +21,6 @@ public class SearchLucene : ISearchLucene,IDisposable
     {
         this.path = path;
     }
-
     public void IndexWriterCreating()
     {
         var idDirectory = Path.Combine(Environment.CurrentDirectory, path);
@@ -44,12 +36,13 @@ public class SearchLucene : ISearchLucene,IDisposable
     public List<string> Search(string searchTerm)
     {
         if (string.IsNullOrEmpty(searchTerm)) return new List<string>();
+        searchTerm = searchTerm.Trim();
         var spellDir = Path.Combine(Environment.CurrentDirectory, "/data/spell");
         var spellcheckerDirectory = FSDirectory.Open(spellDir);
         spellChecker = new SpellChecker(spellcheckerDirectory);
         var indexReader = DirectoryReader.Open(indexDirectory);
         spellChecker.IndexDictionary(new LuceneDictionary(indexReader,"name"), new IndexWriterConfig(LuceneVersion.LUCENE_48, new StandardAnalyzer(LuceneVersion.LUCENE_48)), true);
-        string[] queryWords = searchTerm.Split(' ');
+        string[] queryWords = searchTerm.Split(' ', StringSplitOptions.RemoveEmptyEntries);
         for (int i = 0; i < queryWords.Length; i++)
         {
             string[] suggestions = spellChecker.SuggestSimilar(queryWords[i], 1);
@@ -61,10 +54,9 @@ public class SearchLucene : ISearchLucene,IDisposable
         spellcheckerDirectory.Dispose();
         string correctedSearchTerm = string.Join(" ", queryWords);
         var resultsList = new List<string>();
-        
         var indexSearcher = new IndexSearcher(indexReader);
         var queryParser = new MultiFieldQueryParser(LuceneVersion.LUCENE_48, new string[]{"name", "cat"}, analyzer);
-        var query = queryParser.Parse(string.Join(" ", queryWords)+"*");
+        var query = queryParser.Parse(string.Join(" ", correctedSearchTerm)+"*");
         var topDocs = indexSearcher.Search(query,null, 10);
         foreach (var topDoc in topDocs.ScoreDocs)
         {
@@ -76,16 +68,13 @@ public class SearchLucene : ISearchLucene,IDisposable
     }
     public void AddDocument(Document doc)
     {
-        
         indexWritter.AddDocument(doc);
         indexWritter.ForceMerge(1);
         indexWritter.Commit();
-        
     }
 
     public void Dispose()
     {
         indexWritter.Dispose();
-
     }
 }
